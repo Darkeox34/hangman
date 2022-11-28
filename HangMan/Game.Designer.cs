27fc31a8a59ec9930719ec23229e7611793b7ccf
Darkeox34 <antonio.consoli2003@gmail.com>
2022-11-28 14:04:31 +0100

    Menu Finished + Added words from txt file

--- HangMan/Game.Designer.cs
+++ HangMan/Game.Designer.cs
@@ -29,20 +29,36 @@ namespace HangMan
         /// </summary>
         private void InitializeComponent()
         {
+            this.wordToBeGuessed = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // wordToBeGuessed
+            // 
+            this.wordToBeGuessed.AutoSize = true;
+            this.wordToBeGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordToBeGuessed.Location = new System.Drawing.Point(-4, 9);
+            this.wordToBeGuessed.Name = "wordToBeGuessed";
+            this.wordToBeGuessed.Size = new System.Drawing.Size(210, 73);
+            this.wordToBeGuessed.TabIndex = 0;
+            this.wordToBeGuessed.Text = "label1";
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(690, 540);
+            this.Controls.Add(this.wordToBeGuessed);
             this.Name = "Game";
             this.Text = "Game";
             this.Load += new System.EventHandler(this.Game_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label wordToBeGuessed;
     }
 }
--- HangMan/Game.cs
+++ HangMan/Game.cs
@@ -12,16 +12,34 @@ namespace HangMan
 {
     public partial class Game : Form
     {
-        string[] words = new string[58109];
-
+        string[] words = System.IO.File.ReadAllLines(@"wordlist.txt");
+        string word;
+        Random rnd = new Random();
         public Game()
         {
             InitializeComponent();
         }
 
+        private string newWord()
+        {
+            string word;
+            int r = rnd.Next(0, 50000);
+            word = words[r];
+            return word;
+        }
+
+        private void lose()
+        {
+            newWord();
+        }
+
         private void Game_Load(object sender, EventArgs e)
         {
+ 
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
         }
     }
 }
